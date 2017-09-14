e80212db75316b66123a6eec8405379d8f00fc75
AOSauron <guillaume.garcia@telecomnancy.net>
2017-09-14 12:52:46 +0200

    add main.py

---
+++ src/__main__.py
@@ -0,0 +1,12 @@
+"""
+	Main module for EALPR front software
+	@Author : Guillaume Garcia / AOSauron
+"""
+
+import argparse
+
+print 'Initializing EALPR ...'
+
+PARSER = argparse.ArgumentParser()
+
+exit(0)
